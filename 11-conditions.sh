4c220be2184502997e30625dcfc3951e6bdc03ff
Nageswar <nagesh23479@gmail.com>
2026-01-28 21:36:26 +0530

    final testing

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -7,9 +7,9 @@ NUMBER=$1
 # -eq -> equal to 
 # -ne -> not equal
 
-if [$NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is greater than 20"
-elif [$NUMBER -eq 20]; then
+elif [ $NUMBER -eq 20 ]; then
     echo "Given number: $NUMBER is equal to 20"
 else
     echo "Given number: $NUMBER is less than 20"
