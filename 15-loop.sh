b920da08c3b8ab2ac6f4de59fc9ce91490d95a66
Nageswar <nagesh23479@gmail.com>
2026-01-29 10:37:25 +0530

    adding more scripts

---
+++ 15-loop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/logs/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script from root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+    echo "$2 .. Failure" | tee -a $LOGS_FILE
+    exit 1
+else 
+    echo "$2 .. Successs" | tee -a $LOGS_FILE
+
+fi
+}
+
+for package in $@
+do 
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? "$Package installation"
+done
