224e63b6afc031d7bc1f42ce3b15ec7590e69145
Nageswar <nagesh23479@gmail.com>
2026-01-29 10:22:52 +0530

    adding more scripts

---
+++ 14-logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+        echo "$2 .. Failure" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 .. Success" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
