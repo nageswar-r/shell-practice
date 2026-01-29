f68fa5190d623ddcc16389ccd365f55feb3c3fb0
Nageswar <nagesh23479@gmail.com>
2026-01-29 09:24:37 +0530

    adding more scripts

---
+++ 12-installation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo " Please run this script with root user "
+    exit 1
+fi
+
+echo "Installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx.... Failure"
+    exit 1
+else 
+    echo "Installing Nginx... Success"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+   echo "Installing mysql.... Failure"
+    exit 1
+else 
+    echo "Installing mysql... Success"
+fi
+dnf install nodejs -y
+
+if [ $? -ne 0 ]
+   echo "Installing nodejs.... Failure"
+    exit 1
+else 
+    echo "Installing nodejs... Success"
+fi
