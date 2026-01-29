f2ef7fdee70c2d89c67466b1549ad4d2c6dace59
Nageswar <nagesh23479@gmail.com>
2026-01-29 10:14:02 +0530

    adding more scripts

--- 13-functions.sh
+++ 13-functions.sh
@@ -2,13 +2,13 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo " Please run this script with root user"
     exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0]; then
+    if [ $1 -ne 0 ]; then
         echo "$2 .. Failure"
         exit 1
     else
