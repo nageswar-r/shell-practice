370905f5ee40c133e7f75cb1761f9a0e63aebc55
Nageswar <nagesh23479@gmail.com>
2026-02-05 09:47:12 +0530

    old log delete

--- 17-log-delete.sh
+++ 17-log-delete.sh
@@ -9,6 +9,6 @@ echo "$LOGS_DIR does not exist"
 exit 1
 fi
 
-FILES_TO_DELETE=$(find "$LOGS_DIR" -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
