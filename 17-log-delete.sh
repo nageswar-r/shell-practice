9ea46daed6df0ab0b8d7a04edc3a689db5d7a8a4
Nageswar <nagesh23479@gmail.com>
2026-02-05 09:54:11 +0530

    old log delete

--- 17-log-delete.sh
+++ 17-log-delete.sh
@@ -12,3 +12,16 @@ fi
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+if [ -z $FILES_TO_DELETE ]; then
+
+echo "No files older than 14 days to delete"
+
+else 
+    while IFS= read -r filepath; do
+
+    echo "Deleting file: $filepath"
+    rm -f $filepath
+    echo "Deleted file: $filepath"
+    done <<<$FILES_TO_DELETE
+fi
